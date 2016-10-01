9594cf40896013ddc74681521318bc09fc2848c4
Dante <dante.alighierimds@gmail.com>
2016-10-01 09:35:28 -0300

    Create courses controller with new path

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,19 @@
+class CoursesController < ApplicationController
+  respond_to :html
+
+  def new
+    @course = Course.new
+    respond_with @course
+  end
+
+  def create
+    @course = Course.create(course_params)
+    respond_with @course, location: root_path
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description, :status)
+  end
+end
---
+++ spec/controllers/courses_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe CoursesController, type: :controller do
+  describe '#new' do
+    before { get :new }
+    it { expect(assigns(:course)).to be_an_instance_of(Course) }
+  end
+
+  describe '#create' do
+    subject(:create_course) { post :create, course: course_params }
+
+    context 'when success' do
+      let(:course_params) { attributes_for(:course) }
+
+      it { expect{ create_course }.to change(Course, :count).by(1) }
+
+      it 'course created' do
+        create_course
+        expect(assigns(:course).persisted?).to be_truthy
+      end
+    end
+
+    context 'when error' do
+      let(:course_params) { attributes_for(:course, name: '') }
+
+      it { expect{ create_course }.to change(Course, :count).by(0) }
+
+      it 'course not created' do
+        create_course
+        expect(assigns(:course).persisted?).to be_falsey
+      end
+
+      it 'return course errors' do
+        create_course
+        expect(assigns(:course).errors.messages)
+          .to eq({:name=>['não pode ficar em branco']})
+      end
+    end
+  end
+end
